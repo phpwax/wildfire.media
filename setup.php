f7c455e2ceab6fecf9991e83ce1c4060efcd1b79
Charles Marshall <charles@oneblackbear.com>
2013-02-06 12:37:19 +0000

    forgot about adding those in

--- setup.php
+++ setup.php
@@ -4,13 +4,31 @@ CMSApplication::register_module("media", array("display_name"=>"Media", "link"=>
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
 AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
-AutoLoader::$plugin_array[] = array("name"=>"wildfire.categories","dir"=>__DIR__);
+AutoLoader::$plugin_array[] = array("name"=>"wildfire.media","dir"=>__DIR__);
 
 
 if(defined("CONTENT_MODEL")){
   WaxEvent::add(CONTENT_MODEL.".setup", function(){
     $model = WaxEvent::data();
-    if(!$model->columns['media']) $model->define("media", "ManyToManyField", array('target_model'=>'WildfireMedia', 'group'=>'relationships'));
+    if(!$model->columns['media']) $model->define("media", "ManyToManyField", array('target_model'=>'WildfireMedia', 'group'=>'media'));
   });
 }
+
+
+WildfireMedia::$classes[] = 'WildfireDiskFile';
+//set the default media types for uploads
+WildfireMedia::$allowed  = array(
+                            'jpg'=>'WildfireDiskFile',
+                            'jpeg'=>'WildfireDiskFile',
+                            'JPG'=>'WildfireDiskFile',
+                            'png'=>'WildfireDiskFile',
+                            'gif'=>'WildfireDiskFile',
+                            'txt'=>'WildfireDiskFile',
+                            'doc'=>'WildfireDiskFile',
+                            'xls'=>'WildfireDiskFile',
+                            'zip'=>'WildfireDiskFile',
+                            'pdf'=>'WildfireDiskFile',
+                            'webp'=>'WildfireDiskFile'
+                          );
+
 ?>
