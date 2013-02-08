be7e201f881b8014fe4a85593d011b0a1872ef85
Ross Riley <riley.ross@gmail.com>
2013-02-08 16:22:52 +0000

    cleanup of assets

--- ini.php
+++ ini.php
@@ -1,3 +1,6 @@
 <?
+AutoLoader::register_assets("javascripts/wildfire.media",__DIR__."/assets/javascripts/", "/*.js");
+
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 ?>
