845fc7a2518d29a794bda8b0bb379f5f31e21798
Ross Riley <riley.ross@gmail.com>
2013-02-08 21:49:03 +0000

    and the other register

--- ini.php
+++ ini.php
@@ -1,5 +1,6 @@
 <?
 AutoLoader::register_assets("javascripts/wildfire.media",__DIR__."/assets/javascripts/wildfire.media", "/*.js");
+AutoLoader::register_assets("stylesheets/wildfire.media",__DIR__."/assets/stylesheets/wildfire.media", "/*.css");
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
 AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
