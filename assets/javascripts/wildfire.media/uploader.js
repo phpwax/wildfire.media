06f7374a4cd0cd4973d740fa41660be21da57b72
Ross Riley <riley.ross@gmail.com>
2013-02-08 21:13:38 +0000

    work on upload block

--- assets/javascripts/wildfire.media/uploader.js
+++ assets/javascripts/wildfire.media/uploader.js
@@ -6,31 +6,37 @@ $(document).ready(function() {
 var wildfire_uploader = {
   
   dialog_selector: "#wildfire_uploader",
-  dialog_template: '<div id="wildfire_uploader"></div>',
   progress_template: '<div id="progress"><div class="bar" style="width: 0%;"></div></div>',
   url: "/admin/media/upload",
   
   init: function() {
-    this.setup_listener();
     this.inject_template();
+    this.setup_listener();
   },
   
   setup_listener: function() {
-    $('body').fileupload({
-      dataType: 'json',
+    
+    $('#fileupload').fileupload({
+      
       url: this.url,
+      
       progressall: function (e, data) {
         var progress = parseInt(data.loaded / data.total * 100, 10);
-        $('#progress .bar').css('width',progress + '%');
+        $('.progress .bar').css('width',progress + '%');
       },
+      
       add: function (e, data) {
         data.context = $('<p/>').text('Uploading...').appendTo(document.body);
         data.submit();
       },
+      
       done: function (e, data) {
         data.context.text('Upload finished.');
       }
+      
     });
+    
+ 
   },
   
   inject_template: function() {
