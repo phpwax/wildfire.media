02041485d62422e8ebb6c612054b47a5d3272014
mikenosko <michal@oneblackbear.com>
2013-05-16 17:47:33 +0100

    running select2 on select

--- assets/javascripts/wildfire.media/embedded.js
+++ assets/javascripts/wildfire.media/embedded.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $.get(url, function(response){
       base.append(response);
       wildfire_media.init();
-      $('.media-type-selector').select2({allowClear:true});
+      $('select.media-type-selector').select2({allowClear:true});
     });
     
   }
