47823dfe131437e750920e1bc4b0ca052046e058
Ross Riley <riley.ross@gmail.com>
2013-11-03 11:06:12 +0000

    wrap the target

--- assets/javascripts/wildfire.media/jquery.html5.uploader.js
+++ assets/javascripts/wildfire.media/jquery.html5.uploader.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function($){
 			"right"		: "0px",
 			"z-index"	: "9999"
 		});
-		$("body").prepend(droptarget);
+		$("body div").wrapAll(droptarget);
 
 		jQuery.event.props.push("dataTransfer");
 		var drop_area = jQuery("#file-drop-target"),
